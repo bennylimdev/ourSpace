fe58e56c7bc153d2d87dbb7bcf631f5c694b2925
Ben Lim <finaldessert@gmail.com>
2023-12-18 17:43:13 -0500

    set up migrations for Users Table

--- db/migrate/20231218221727_create_users.rb
+++ db/migrate/20231218221727_create_users.rb
@@ -1,8 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+            t.string :first_name, null: false
+            t.string :last_name, null: false
+            t.string :email, null: false, unique: true
+            t.string :username, null: false, unique: true
+            t.string :bio
+            t.string :birthday
+            t.string :password_digest, null: false
+            t.string :session_token, null: false, unique: true
       t.timestamps
     end
+
+    add_index :users, :email
+    add_index :users, :session_token
+    add_index :users, :username
   end
 end
