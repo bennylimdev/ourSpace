e39ca35080b6af1a2fe4f40ee49801196c1f297a
Ben Lim <finaldessert@gmail.com>
2023-12-18 17:35:48 -0500

    Add users controller with error handling and correct permissions to prevent SQL injections

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,36 @@
+class Api::UsersController < ApplicationController
+    def index
+        @users = User.all
+        render :index
+    end
+
+    def show 
+        @user = User.find(params[:id])
+        render :show
+    end
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 401
+        end
+    end
+
+    private 
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :email, :password, :bio, :birthday) 
+    end
+end
