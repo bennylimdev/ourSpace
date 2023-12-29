0f1609e599431afa3456150f2fb5767b2c5215b7
Ben Lim <finaldessert@gmail.com>
2023-12-29 09:45:32 -0500

    feat: posts routes; posts ajax methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:index, :create, :show, :update]
+    resources :posts
   end
 
   root to: 'static_pages#root'
