aeaa2e76e38598a378e9133243600365ef88fe1b
Ben Lim <finaldessert@gmail.com>
2024-01-04 17:14:09 -0500

    feat: setup aws env

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,8 @@ class User < ApplicationRecord
     has_many :postlikes,
         foreign_key: :author_id,
         class_name: :Postlike
+
+    has_one_attached :profile_pic, dependent: destroy
     
     def self.find_by_credentials(email, password)
         user = User.find_by(email: email)
